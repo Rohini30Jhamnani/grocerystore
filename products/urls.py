360e42c88a306e797cb102323fc23ef1ea8f9cbf
Rohini30Jhamnani <rohinirapid@gmail.com>
2022-05-30 22:59:32 +0530

    First commit Message

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('cart/', views.cart),
+    path('thank_you/', views.thank_you),
+    path('payment/', views.payment),
+
+]
